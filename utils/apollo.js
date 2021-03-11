c67ea7b958b5a4329e191fb4c17bf962fec807f9
Matt Malone <mattagape@me.com>
2021-03-11 13:37:32 +0700

    Setting up gatsby-source-graphql

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { ApolloClient, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import fetch from "isomorphic-fetch";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+
+// // This 'dotenv' creates the 'fs' error.  Do whatever it takes to fix dotenv in order to use process.env.... etc...
+// dotenv.config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
+
+const cache = new InMemoryCache();
 
 export const client = new ApolloClient({
+	cache: cache,
 	uri: 'https://hasura-heroku-based.herokuapp.com/v1/graphql',
 	headers: {
-    'x-hasura-admin-secret': `oi2mmraOYAPLzTjwQaLYeQyKKITjNoIxrUT3DSm0fPGYK2leWLbbWEiSiwa5O9GN`, // `${process.env.GATSBY_HASURA_GRAPHQL_ADMIN_SECRET}`, 
+    'x-hasura-admin-secret': `oi2mmraOYAPLzTjwQaLYeQyKKITjNoIxrUT3DSm0fPGYK2leWLbbWEiSiwa5O9GN`, // `${process.env.GATSBY_HASURA_GRAPHQL_ADMIN_SECRET}`,
   },
 	fetch,
 });
