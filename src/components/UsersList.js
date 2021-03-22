bdb3b06f6a161c4a37afd3597ebcc87884b5aa14
Matt Malone <mattagape@me.com>
2021-03-23 03:28:41 +0700

    Working on ws

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -28,7 +28,7 @@ const UsersList = ({ FbName }) => {
   return(
     <ul>
       {data.users.map(user => (
-        <li key={user.id}>{user.username} <span style={{ color: `#d4c9c9`, fontSize: `50%` }}>...by {FbName}</span></li>
+        <li key={user.id}>{user.username} <span style={{ color: `#c7b7b7`, fontSize: `60%` }}>...by {FbName}</span></li>
       ))}
     </ul>
   )
