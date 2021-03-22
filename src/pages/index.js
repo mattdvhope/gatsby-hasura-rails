886eb0c822e61d67499eb0f303aec2a2930006bd
Matt Malone <mattagape@me.com>
2021-03-22 13:05:55 +0700

    FB login setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,11 @@ const IndexPage = () => {
     const code = url_with_code ? url_with_code[2] : null;
 
     const token = await getAccessToken(code)
-    const appToken = await getAppAccessToken()
-    const objectFromDebug = await inspectAccessToken(token, appToken)
-    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
+    // const appToken = await getAppAccessToken()
+    // const objectFromDebug = await inspectAccessToken(token, appToken)
+    // const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
 
-    console.log(profile_of_person);
+    console.log(token);
 
     // handleLogin(profile_of_person)
   });
