a4c8c5b24c63041210b3d22edc734ee3e7f9dbf4
Matt Malone <mattagape@me.com>
2021-03-23 02:46:56 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -14,21 +14,14 @@ const SecondPage = () => {
     const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
     const code = url_with_code ? url_with_code[2] : null
 
-    // if (!isLoggedIn() && code) {
-
+    if (!JSON.parse(window.localStorage.getItem("user")).name) {
 	    const token = await getAccessToken(code)
 	    const appToken = await getAppAccessToken()
 	    const objectFromDebug = await inspectAccessToken(token, appToken)
 	    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
 	    handleLogin(profile_of_person);
-
-	    if (JSON.parse(window.localStorage.getItem("user")).name) {
-	    	console.log("SOMETHING HERE!!!!")
-      	setName(profile_of_person.name)
-	    }
-
-
-    // }
+    	setName(profile_of_person.name)
+    }
 
   }); // useEffect
 
