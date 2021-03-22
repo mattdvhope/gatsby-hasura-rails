5097480de79def37a2f533350bb1b88717ab3190
Matt Malone <mattagape@me.com>
2021-03-22 21:31:33 +0700

    FB Login setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import UserStuff from "../components/UserStuff"
 import { isLoggedIn, getUser, handleLogin } from "../utils/auth"
 import { FbLoginURL } from "../utils/FbLoginURL"
-import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
 
 const IndexPage = () => {
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import UserStuff from "../components/UserStuff"
 import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
 
 const SecondPage = () => {
@@ -23,8 +23,7 @@ const SecondPage = () => {
 	return (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <UserStuff/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )}
