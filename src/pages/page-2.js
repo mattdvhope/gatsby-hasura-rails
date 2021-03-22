b1d17406d47991e3855bf671fe667c0b43f8d471
Matt Malone <mattagape@me.com>
2021-03-22 23:38:59 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,7 +10,7 @@ const SecondPage = () => {
 
 	const [name, setName] = useState(null);
 
-  const bringForthTheFbProfile = async () => {
+  useEffect(async () => {
     const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
     const code = url_with_code ? url_with_code[2] : null
 
@@ -20,18 +20,16 @@ const SecondPage = () => {
 	    const objectFromDebug = await inspectAccessToken(token, appToken)
 	    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
 	    handleLogin(profile_of_person)
-	  } else {
-			setName(getUser().name);
 	  }
-  }
+  }); // useEffect
 
 	return (
-  <Layout>
-    <SEO title="Page two" />
-    <h3>Hi {name}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
-    <UserStuff/>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>)
+	  <Layout>
+	    <SEO title="Page two" />
+	    <h3>Hi {isLoggedIn() ? getUser().name : null}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
+	    <UserStuff/>
+	    <Link to="/">Go back to the homepage</Link>
+	  </Layout>)
 
 }
 
