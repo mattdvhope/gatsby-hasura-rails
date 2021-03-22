244f31ef059914f2053db7d87e8e6d9c96398dea
Matt Malone <mattagape@me.com>
2021-03-22 21:24:58 +0700

    FB Login setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,16 +8,6 @@ import { FbLoginURL } from "../utils/FbLoginURL"
 import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
 
 const IndexPage = () => {
-
-  useEffect(async () => {
-    const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
-    const code = url_with_code ? url_with_code[2] : null
-    const token = await getAccessToken(code)
-    const appToken = await getAppAccessToken()
-
-    console.log(token);
-    console.log(appToken);
-  }); // useEffect
 
   return (
     <Layout>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -12,9 +12,12 @@ const SecondPage = () => {
     const code = url_with_code ? url_with_code[2] : null
     const token = await getAccessToken(code)
     const appToken = await getAppAccessToken()
+    const objectFromDebug = await inspectAccessToken(token, appToken)
+    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
 
     console.log(token);
     console.log(appToken);
+    console.log(profile_of_person)
   }); // useEffect
 
 	return (
