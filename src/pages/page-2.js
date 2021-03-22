c35aee9ccff180c55667d2ab3704f31da3078be6
Matt Malone <mattagape@me.com>
2021-03-23 00:55:42 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -14,7 +14,7 @@ const SecondPage = () => {
     const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
     const code = url_with_code ? url_with_code[2] : null
 
-    if (!isLoggedIn()) {
+    if (!isLoggedIn() && code) {
 	    const token = await getAccessToken(code)
 	    const appToken = await getAppAccessToken()
 	    const objectFromDebug = await inspectAccessToken(token, appToken)
