7e23bf89296d1c33bb2d8d50a9a1a9f3737769ec
Matt Malone <mattagape@me.com>
2021-03-22 23:22:51 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,36 +3,32 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import UserStuff from "../components/UserStuff"
+import { isLoggedIn, getUser, handleLogin } from "../utils/auth"
 import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
 
 const SecondPage = () => {
 
 	const [name, setName] = useState(null);
 
-	let namely;
-
   const bringForthTheFbProfile = async () => {
     const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
     const code = url_with_code ? url_with_code[2] : null
-    const token = await getAccessToken(code)
-    const appToken = await getAppAccessToken()
-    const objectFromDebug = await inspectAccessToken(token, appToken)
-    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
 
-    console.log(token);
-    console.log(appToken);
-    console.log(profile_of_person)
-
-    setName(profile_of_person.name);
-    return name;
+    if (!isLoggedIn() && code) {
+	    const token = await getAccessToken(code)
+	    const appToken = await getAppAccessToken()
+	    const objectFromDebug = await inspectAccessToken(token, appToken)
+	    const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
+	    handleLogin(profile_of_person)
+	  } else {
+			setName(getUser().name);
+	  }
   }
-
-
 
 	return (
   <Layout>
     <SEO title="Page two" />
-    <h3>Hi {bringForthTheFbProfile()}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
+    <h3>Hi {name}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
     <UserStuff/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>)
