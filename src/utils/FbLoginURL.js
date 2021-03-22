afc02769202cdf636f083111b089c6b9a90c27b7
Matt Malone <mattagape@me.com>
2021-03-22 21:05:57 +0700

    FB Login setup

--- src/utils/FbLoginURL.js
+++ src/utils/FbLoginURL.js
@@ -1,8 +1,8 @@
-import { makeState } from './makeState';
+import { makeState, makeNum } from './makeState';
 
 // This is URL provided by FB Platform for logging into this app via FB.
 export const FbLoginURL = () => {
   return `
-    https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state={st=state${makeState(6)},ds=${makeState(9)}}&locale=th_TH
+    https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state={st=state${makeState(6)},ds=${makeNum(9)}}&locale=th_TH
   `
 }
--- src/utils/makeState.js
+++ src/utils/makeState.js
@@ -7,3 +7,13 @@ export const makeState = (length) => {
   }
   return result;
 }
+
+export const makeNum   = (length) => {
+  let result           = '';
+  let characters       = '0123456789';
+  let charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
