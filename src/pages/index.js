0889842635d5fb9bd944fdf3e5b3a8ebeb6a0da8
Matt Malone <mattagape@me.com>
2021-03-22 20:22:27 +0700

    FB Login setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,10 @@ const IndexPage = () => {
     const url_with_code = window.location.search.match(/(code=)(.*)(?=&state)/)
     const code = url_with_code ? url_with_code[2] : null
     const token = await getAccessToken(code)
+    const appToken = await getAppAccessToken()
 
     console.log(token);
+    console.log(appToken);
   }); // useEffect
 
 
