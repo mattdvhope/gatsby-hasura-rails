1b3322461d38c41d4ea8f70ffd286abcf4c87042
Matt Malone <mattagape@me.com>
2021-03-23 01:24:49 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -18,10 +18,10 @@ const SecondPage = () => {
     const appToken = await getAppAccessToken()
     const objectFromDebug = await inspectAccessToken(token, appToken)
     const profile_of_person = await getUserProfile(objectFromDebug.data.user_id, token)
+    console.log(profile_of_person);
+    handleLogin(profile_of_person)
 
     if (!isLoggedIn()) {
-	    console.log(profile_of_person);
-	    handleLogin(profile_of_person)
 	    setName(getUser().name)
 	  }
 
