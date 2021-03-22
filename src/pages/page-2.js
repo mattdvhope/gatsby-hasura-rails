6d7e65dc8a2e9b0554518213d989697b4e00a251
Matt Malone <mattagape@me.com>
2021-03-22 22:39:07 +0700

    Working on ws

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -32,7 +32,7 @@ const SecondPage = () => {
 	return (
   <Layout>
     <SEO title="Page two" />
-    <h3>Hi {setName(namely)}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
+    <h3>Hi {namely}, Here is some interesting content for a two or more people to chat about while interacting on this web page.  They won't have to switch back and forth between this page and Facebook Messenger or LINE.  They can have all of their interaction, discipleship, relationship-development, content-sharing, etc, etc, right here on this page!!</h3>
     <UserStuff/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>)
