b5cce539a30404ec594f35e21d450d5019f5d961
Matt Malone <mattagape@me.com>
2021-03-22 20:45:18 +0700

    FB Login setup

--- src/utils/FbLoginURL.js
+++ src/utils/FbLoginURL.js
@@ -3,6 +3,6 @@ import { makeState } from './makeState';
 // This is URL provided by FB Platform for logging into this app via FB.
 export const FbLoginURL = () => {
   return `
-    https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state=${makeState(10)}&locale=th_TH
+    https://www.facebook.com/v10.0/dialog/oauth?client_id=${process.env.GATSBY_FB_APP_CLIENT_ID}&redirect_uri=${process.env.GATSBY_API_URL}&state={st=state${makeState(6)},ds=${makeState(9)}}&locale=th_TH
   `
 }
