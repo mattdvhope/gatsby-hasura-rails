65932f905ecba12239e84491197e9c7d9a95c7e3
Matt Malone <mattagape@me.com>
2021-03-24 15:55:01 +0700

    Util work

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -7,9 +7,9 @@ const UserProfile = ({ FbUser }) => {
   return (
   <div>
     <p>This is who you are according to Facebook...</p>
-    <p>Full Name: {FbUser.name}</p>
-    <p>First Name: {FbUser.first_name}</p>
-    <p>Last Name: {FbUser.last_name}</p>
+    <p>Full Name: {user.name}</p>
+    <p>First Name: {user.first_name}</p>
+    <p>Last Name: {user.last_name}</p>
   </div>
 
 )}
