50a6386c4bb77c188adb749b6b7ef6d24c2172b4
Matt Malone <mattagape@me.com>
2021-03-24 14:32:55 +0700

    commented

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Dashboard from "../components/Dashboard"
 import { isLoggedIn, getUser, handleLogin } from "../utils/auth"
-import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
+// import { getAccessToken, getAppAccessToken, inspectAccessToken, getUserProfile } from "../utils/FBLoginValidations"
 import { GetCodeFromUrl, GetFbUserProfile } from "../utils/GetFbUserProfile";
 
 const SecondPage = () => {
