74b249b5169184b742b2118faca54dadb7cc1fad
Matt Malone <mattagape@me.com>
2021-03-26 14:28:00 +0700

    Removed UserProfile

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,14 +5,16 @@ import { getUser } from "../utils/auth";
 
 const Dashboard = () => {
 
+
+
 	return (
 		<div>
 	    <div style={{ margin: `2rem auto`, width: `550px` }}>
 	      <UserRegistration/>
 	    </div>
-			<div style={{ margin: `2rem auto`, width: `550px` }}>
+	{/* <div style={{ margin: `2rem auto`, width: `550px` }}>
 	      <UserProfile/>
-	    </div>
+	    </div>  */}		
 	  </div>
 	)
 }
