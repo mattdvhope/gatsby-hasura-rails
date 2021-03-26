2dfdf8619ca1c55482bb735dff886698ed9ccf53
Matt Malone <mattagape@me.com>
2021-03-26 23:11:25 +0700

    User registration

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -19,7 +19,16 @@ const SecondPage = () => {
     }
   }); // useEffect
 
-  const timeNow = new Date();
+  const days_passed = (dt) => {
+	  const current = new Date(dt.getTime());
+	  const previous = new Date(2021, 0, 1);
+
+	  return Math.ceil((current - previous + 1) / 86400000);
+	}
+
+	const d = new Date();
+
+	const timeNow = days_passed(new Date(d.getFullYear(), d.getUTCMonth(), d.getUTCDate()));
 
 	if (fbId || getUser().id) {
 		return (
