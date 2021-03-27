8dec132e659fea7f08180982cf450de312c5f67e
Matt Malone <mattagape@me.com>
2021-03-27 09:20:20 +0700

    User registration

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { getUser } from "../utils/auth"
 import { gql, useMutation } from '@apollo/client';
 import UserProfile from "./UserProfile";
@@ -35,28 +35,22 @@ const UserRegistration = ({ timeNow }) => {
   const fbUser = getUser();
   const [addFbUser, { data }] = useMutation(ADD_FB_USER);
 
-  useEffect(() => {
-		window.onload = function() {
-	  	addFbUser({
-		  	variables: {
-		  	 fb_id: fbUser.id,
-		  	 first_name: fbUser.first_name,
-		  	 last_name: fbUser.last_name,
-		  	 name: fbUser.name,
-		  	 picture_url: fbUser.picture.data.url,
-		  	 login_time: timeNow
-		  	}
-		  });
-		  console.log("inside useEffect, after addFbUser")
-		};
-  }); // useEffect
-
-
-
   return (
 		<div id="mainDiv">
 	    <div style={{ margin: `2rem auto`, width: `550px` }}>
-	      <p>Time: {timeNow}</p>
+	      <p onClick={e => {
+	      		e.preventDefault();
+				  	addFbUser({
+					  	variables: {
+					  	 fb_id: fbUser.id,
+					  	 first_name: fbUser.first_name,
+					  	 last_name: fbUser.last_name,
+					  	 name: fbUser.name,
+					  	 picture_url: fbUser.picture.data.url,
+					  	 login_time: timeNow
+					  	}
+					  });
+	      }}>Time: {timeNow}</p>
 	      <UserProfile />
 	    </div>
 	  </div>
